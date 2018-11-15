9286963dde0c5c958a8a48b1af06a77b470a82b5
sathvik <sathvik@CORP.AMAZONWORKSPACES.COM>
2018-11-15 23:35:53 +0530

    "First Commit"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatCardModule, MatChipsModule, MatToolbarModule, MatTableModule} from '@angular/material'
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatTableModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
